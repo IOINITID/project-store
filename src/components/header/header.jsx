a737697eb17b09a9fee0831e48374d9d96ad71eb
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-11-25 21:37:24 +0300

    Added icon files and updated header styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import './header.scss';
+import LogoIcon from '../../assets/images/logo-icon.svg';
+import HeartFilledIcon from '../../assets/images/heart-filled-icon.svg';
+import CartIcon from '../../assets/images/cart-icon.svg';
+import MenuIcon from '../../assets/images/menu-icon.svg';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <img
-          className="header__logo"
-          src="https://via.placeholder.com/184"
-          width="184"
-          height="40"
-          alt="Логотип Project Store."
-        />
-        <a className="header__button" href="#">Каталог</a>
-
+        <LogoIcon className="header__logo" width="184" height="40" aria-label="Логотип Project Store." />
+        <a className="header__button" href="#">
+          <MenuIcon className="header__button-icon" width="16" height="16" aria-label="Иконка меню." />
+          Каталог
+        </a>
         <div className="header__info">
           <div className="header__favourites">
-            <span>2</span>
+            <HeartFilledIcon className="header__favourites-icon" width="24" height="24" />
+            <span className="header__favourites-count">2</span>
           </div>
           <div className="header__cart">
-            <span>2</span>
-            <span>2 550 ₽</span>
+            <CartIcon className="header__cart-icon" width="32" height="32" />
+            <span className="header__cart-count">2</span>
+            <span className="header__cart-price">2 550 ₽</span>
           </div>
         </div>
       </div>
